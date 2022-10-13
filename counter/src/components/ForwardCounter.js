d9ea13fe1d5101e94734d3107a532c48d3a8d5f7
Mohish Khadse <97750399+MohishKhadse55@users.noreply.github.com>
2022-10-13 18:36:38 +0530

    Add files via upload

---
+++ counter/src/components/ForwardCounter.js
@@ -0,0 +1,15 @@
+
+
+import Card from './Card';
+import useCounter from "../Hooks/use-counter";
+
+const ForwardCounter = () => {
+  const counter = useCounter()
+
+
+
+
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;

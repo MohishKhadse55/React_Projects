e55c64c96390354e81888f42d76b8963c1e12c30
Mohish Khadse <97750399+MohishKhadse55@users.noreply.github.com>
2022-10-21 22:01:31 +0530

    Add files via upload

---
+++ Shoping Cart Using Advance Redux/src/store/cart-slice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      console.log(newItem);
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+        console.log(state.items[0]);
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      console.log(state.totalQuantity);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id); // here we are removing the item for which id matches  or filtering the items which are not
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
